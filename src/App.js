92af05bb5d618235cae2605555c3383e062ac0ff
mina.h <mina.h@amaryllo.us>
2021-02-14 12:28:21 +0800

    <add><feature><change app.js text>

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>oh there!</p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
+                    I was changed on the feature branch
                 </a>
             </header>
         </div>
